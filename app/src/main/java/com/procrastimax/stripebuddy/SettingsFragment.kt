b467bf3331921926dcb800cf6d46c2089271566c
procrastimax <mmeheykeroth@protonmail.com>
2023-03-15 21:42:33 +0100

    Adds checks for port/ endpoint settings

--- app/src/main/java/com/procrastimax/stripebuddy/SettingsFragment.kt
+++ app/src/main/java/com/procrastimax/stripebuddy/SettingsFragment.kt
@@ -2,22 +2,59 @@ package com.procrastimax.stripebuddy
 
 import android.os.Bundle
 import android.text.InputType
+import android.webkit.URLUtil
+import android.widget.Toast
 import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
+
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
 
-        val portNumberPreference : EditTextPreference? = findPreference("port")
-        portNumberPreference?.setOnBindEditTextListener {editText ->
+        val portNumberPreference: EditTextPreference? = findPreference("port")
+        portNumberPreference?.setOnBindEditTextListener { editText ->
             editText.inputType = InputType.TYPE_CLASS_NUMBER
         }
 
-        val endpointPreference : EditTextPreference? = findPreference("endpoint")
-        endpointPreference?.setOnBindEditTextListener {editText ->
+        val endpointPreference: EditTextPreference? = findPreference("endpoint")
+        endpointPreference?.setOnBindEditTextListener { editText ->
             editText.inputType = InputType.TYPE_TEXT_VARIATION_URI
+        }
+
+        endpointPreference?.setOnPreferenceChangeListener { _, newValue ->
+            if (URLUtil.isValidUrl(newValue.toString())) {
+                true
+            } else {
+                Toast.makeText(
+                    context,
+                    "$newValue is not a valid API Endpoint URL/ IP Adress!",
+                    Toast.LENGTH_LONG
+                ).show()
+                false
+            }
+        }
+
+        portNumberPreference?.setOnPreferenceChangeListener { _, newValue ->
+            val portNumber: Int? = try {
+                newValue.toString().toInt()
+            } catch (e: NumberFormatException) {
+                null
+            }
+
+            portNumber?.run {
+                if (this in 1..65535) {
+                    true
+                } else {
+                    Toast.makeText(
+                        context,
+                        "$newValue is not a valid port number!",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    false
+                }
+            } ?: false
         }
     }
 
